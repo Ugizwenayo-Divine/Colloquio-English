f1ad71189f2ef0e9675a5cde04fdd708723e7c8a
Ugizwenayo-Divine <ugizwenayodiny@gmail.com>
2022-02-22 17:47:43 +0200

    codeland challenge 2

--- js/index.js
+++ js/index.js
@@ -1,91 +1,91 @@
-function submit() {
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const phone = document.getElementById("phone");
-  const company = document.getElementById("company");
-  const message = document.getElementById("message");
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const company = document.querySelector("#company");
+const message = document.querySelector("#message");
+const fields = document.querySelectorAll(".field");
 
-  if (name.value == null || name.value == "") {
-    name.classList.add("red-color");
-    name.placeholder = "Write your name and surname";
+const clearFieldError = (element) => {
+  element.classList.remove("input-error");
+  if (
+    element.nextElementSibling &&
+    element.nextElementSibling.classList.contains("error")
+  ) {
+    element.nextElementSibling.style.display = "none";
   }
-  if (company.value == null || company.value == "") {
-    company.classList.add("red-color");
-    company.placeholder = "Write your company name";
-  }
-  if (email.value == null || email.value == "") {
-    email.classList.add("red-color");
-    email.placeholder = "Write an email(example@example.com)";
-  }
-  if (phone.value == null || phone.value == "") {
-    phone.classList.add("red-color");
-    phone.placeholder = "Write your phone number";
-  }
-  if (message.value == null || message.value == "") {
-    message.classList.add("red-color");
-    message.placeholder = "Write your message";
-  }
-}
+};
 
-function reset() {
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const phone = document.getElementById("phone");
-  const company = document.getElementById("company");
-  const message = document.getElementById("message");
-  const fields = document.getElementsByClassName("field");
-
-  for (let i = 0; i < fields.length; i++) {
-    fields[i].classList.remove("red-color");
-    fields[i].value = "";
+const validateEmail = (element) => {
+  if (
+    element.value.length > 0 &&
+    !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(element.value)
+  ) {
+    element.classList.add("input-error");
+    element.nextElementSibling.style.display = "block";
   }
-  document.getElementById("phone-error").style.display = "none";
-  document.getElementById("email-error").style.display = "none";
-  name.placeholder = "Name";
+};
+
+const onChange = ({ target: element }) => {
+  if (element.value.length >= 8 && element.value.length <= 10) {
+    clearFieldError(element);
+  } else {
+    element.classList.add("input-error");
+    element.nextElementSibling.style.display = "block";
+  }
+};
+
+const isNumber = (event) => {
+  const charCode = event.which ? event.which : event.keyCode;
+  if (
+    (charCode > 31 && (charCode < 48 || charCode > 57)) ||
+    event.target.value.length > 9
+  ) {
+    event.preventDefault();
+  }
+};
+
+const validateInput = (element, placeholder) => {
+  if (element.value === null || element.value.trim() === "") {
+    element.classList.add("input-error");
+    element.placeholder = placeholder;
+  }
+};
+
+const submit = () => {
+  validateInput(nameInput, "Write your name and surname");
+  validateInput(company, "Write your company name");
+  validateInput(email, "Write an email (example@example.com)");
+  validateInput(phone, "Write your phone number");
+  validateInput(message, "Write your message");
+
+  if (email.value.length > 0) {
+    validateEmail(email);
+  }
+};
+
+const reset = () => {
+  fields.forEach((el) => {
+    clearFieldError(el);
+    el.value = "";
+  });
+  nameInput.placeholder = "Name";
   email.placeholder = "Email";
   phone.placeholder = "Phone";
   company.placeholder = "Company";
   message.placeholder = "Your message";
-}
+};
 
-function clearField(element) {
-  const fields = document.getElementsByClassName("field");
-  const phoneError = document.getElementById("phone-error");
-  const emailError = document.getElementById("email-error");
-  if (element.name == "phone") {
-    phoneError.style.display = "none";
-  }
-  if (element.name == "email") {
-    emailError.style.display = "none";
-  }
-
-  for (let i = 0; i < fields.length; i++) {
-    if (element.name == fields[i].name) {
-      fields[i].classList.remove("red-color");
+const initialEvents = () => {
+  fields.forEach((el) => {
+    if (el.name !== "phone") {
+      el.addEventListener("keyup", clearFieldError);
+    } else {
+      el.addEventListener("keyup", onChange);
+      el.addEventListener("keypress", isNumber);
     }
-  }
-}
-function onChange(element) {
-  console.log(element.name);
-  const phoneError = document.getElementById("phone-error");
-  const emailError = document.getElementById("email-error");
-  const email = document.getElementById("email");
+  });
+  document.querySelector("#send-btn").addEventListener("click", submit);
+  document.querySelector("#reset-btn").addEventListener("click", reset);
+};
 
-  if (
-    element.name == "phone" &&
-    (element.value.length < 8 ||
-      element.value.length > 10 ||
-      !/^[\d]+$/.test(element.value))
-  ) {
-    phone.classList.add("red-color");
-    phoneError.style.display = "block";
-  }
-  if (
-    element.name == "email" &&
-    element.value.length > 0 &&
-    !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(element.value)
-  ) {
-    email.classList.add("red-color");
-    emailError.style.display = "block";
-  }
-}
+initialEvents();
